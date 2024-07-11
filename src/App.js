db5fcd00691cc14e986e846c6e5f954c39296b52
Viney-Arora <aroraviney2004@gmail.com>
2024-07-11 19:36:45 +0530

    little bit changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import hotel from './hotelbg.jpg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="img">
+        <header className="App-header">
+          <img src={hotel} className="App-logo" alt="logo" />
+          <p>
+            This is VP restaurant website
+          </p>
+          <a
+            className="App-link"
+            href="https://maps.app.goo.gl/1V4pHutFKJu53bPi6"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Location
+          </a>
+        </header>
+      </div>
     </div>
   );
 }
